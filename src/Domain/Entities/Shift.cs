28183135eddd6cd263d3111b25add7c96c3d0315
mati-gif <matiassoria7700@gmail.com>
2024-11-26 20:24:04 -0300

    añadiendo shiftDto y shiftRequest

---
+++ src/Domain/Entities/Shift.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Shift
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Required]
+        public int price { get; set; }
+
+        [Required]
+        public bool pending { get; set; }
+
+        [Required]
+        public bool confirmed { get; set; }
+
+        [Required]
+        public bool isPayabled { get; set; }
+
+        [Required]
+        public User client { get; set; }
+
+        [Required]
+        public User barber { get; set; }
+
+        [Required]
+
+        public BarberShop barberShop { get; set; }
+
+        [Required]
+
+        public List<ServicesAndHaircuts> services { get; set; }
+
+        [Required]
+
+        public DateTime day { get; set; }
+    }
+}
--- src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ src/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da81806dda245424e966fc35a8f7516dd225d32c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d098f10faf98a75e32d025d24d9d09d64cee49c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
