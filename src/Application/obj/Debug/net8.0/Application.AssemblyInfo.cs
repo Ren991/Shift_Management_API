c5eab356fe2962cded210e24804a24bfa3341e27
Ren991 <beccrenzo@gmail.com>
2025-01-04 13:31:20 -0300

    cambiando puerto a 5174

--- src/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40993bddfdbaed44177e47ba5f796b691b60674a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c909673e98d16c90eed318c21f6d5ac75fe4267b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
