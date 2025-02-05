c0042fcc74baaee432b59765faadd12d7eb9a6ff
mati-gif <matiassoria7700@gmail.com>
2025-02-05 20:21:25 -0300

    cambiando como se parsea la fecha y la hora en  CancelShift

--- src/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ src/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9cdf79dd2b645b7f7571164eb34a98f0c959349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11cb2607fe1af4cfe59fd310f2edc604238bfd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
